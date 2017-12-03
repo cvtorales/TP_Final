3de6238780b226c0365fba116f478413d0315771
agustinpiperno <31783815+agustinpiperno@users.noreply.github.com>
2017-12-03 20:36:10 -0300

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -8,6 +8,8 @@ status_t destruir_arreglo_cadenas(char *** campos, size_t n);
 status_t split_message(const char * cadena, char delimitador, char *** arreglo, size_t *l, int cant_campos);
 void destruir_mensaje(void ** mensaje);
 status_t procesar_datos_de_usuario(FILE * archivo, lista_t * lista);
+status_t eliminar_usuarios_por_id(char * argv[CMD_POS_ID], nodo_t * red);
+void eliminar_usuario_por_id(void* dato1,void *argv[CMD_POS_ID]);
 
 /**********************************************************************************/
 
